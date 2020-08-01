38df0d03120c484491acedadc8f328e904ff25ed
AlenaShurakova <ashurakovaa@gmail.com>
2020-08-01 18:08:07 +0300

    Lesson_5 fixing

--- src/Lesson_5/Task_19_M.java
+++ src/Lesson_5/Task_19_M.java
@@ -5,8 +5,6 @@ package Lesson_5;
 текста также могут быть пробелы, но могут и отсутствовать.*/
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Task_19_M {
     public static void main(String[] args) {
@@ -14,12 +12,11 @@ public class Task_19_M {
         System.out.println("Введите строку:");
         String input = scanner.nextLine();
         scanner.close();
-        Pattern pattern = Pattern.compile("[\\p{Punct}]");
-        String[] wordsList = input.split(" ");
+        String inputReplacedPunchMarks = input.replaceAll("[\\p{Punct}]", " ");
+        String[] wordsList = inputReplacedPunchMarks.split(" ");
         int wordscount = 0;
         for (int i = 0; i < wordsList.length; i++) {
-            Matcher matcher = pattern.matcher(wordsList[i]);
-            if (!matcher.matches() && wordsList[i].compareTo("") != 0) {
+            if (wordsList[i].compareTo("") != 0) {
                 wordscount++;
             }
         }
